659545a13509c33d56a2ed1fe402ba43bae2ded1
Rai Miguel <raimiguel_@outlook.com>
2025-05-31 16:38:16 -0300

    Exercicios de vetores 1 e 2

---
+++ exercicios/exercicios_vetores/Ex001.java
@@ -0,0 +1,12 @@
+
+public class Ex001 {
+    public static void main(String[] args) {
+        String [] personagens = {"naruto", "Sasuke", "Shikamaro", "André", "Ino"};
+
+        for(int indice=0; indice < personagens.length; indice++) {
+            System.out.print(personagens[indice]+ " ");
+        
+        }
+        System.out.println("");
+    }
+}
---
+++ exercicios/exercicios_vetores/Ex002.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Ex002 {
+    public static void main(String[] args) {
+        String [] jutsus = {"rasengan", "clone", "invocação"};
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite um jutsu: ");
+        String jutsu = scan.nextLine();
+        boolean existe = false;
+
+        for (int index=0; index < jutsus.length; index++){
+            if (jutsu.equals(jutsus[index])){
+                existe = !existe;
+            }
+        }
+        if (existe) {
+        System.out.println("tem na lista");
+        }
+        else {
+            System.out.println("não tem na lista");
+        }
+    }
+}
