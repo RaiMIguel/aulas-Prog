eb49c9ef18c6bcbc77b6d334a52869a54f995eb4
André Figueiredo <andre.figueiredo@jointecnologia.com.br>
2025-09-27 16:49:05 -0300

    questao 1 de poo

---
+++ exercicios/cem_questoes_poo/questao_1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ninja naruto = new Ninja("naruto uzumaki",12);
+        System.out.println("nome do ninja: " + naruto.getNome());
+        System.out.println("idade do ninja: " + naruto.getIdade());
+
+    }
+}
---
+++ exercicios/cem_questoes_poo/questao_1/Ninja.java
@@ -0,0 +1,21 @@
+public class Ninja {
+    private String nome;
+    private int idade;
+
+    public Ninja(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
