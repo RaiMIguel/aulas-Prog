941502cc2ee5a88cf1a59ded262a8f89a126a06f
Raí Miguel Araújo de Oliveira <raimiguel_@outlook.com>
2025-12-13 14:18:33 -0300

    teste pr

--- exercicios/exercicios_estrutura_dados/listaligada/ListaLigadaNinja.java
+++ exercicios/exercicios_estrutura_dados/listaligada/ListaLigadaNinja.java
@@ -1,12 +1,11 @@
 package exercicios.exercicios_estrutura_dados.listaligada;
 
-
 public class ListaLigadaNinja {
+
     public class NinjaNode {
         String name;
         int powerChakra;
         String village;
-
         NinjaNode next;
 
         public NinjaNode(String name, int powerChakra, String village){
@@ -33,13 +32,16 @@ public class ListaLigadaNinja {
         return size;
     }
 
-    public void insertBeginning(String nameIB,int powerIB, String villageIB){
-        NinjaNode newNinja = new NinjaNode(nameIB,powerIB,villageIB);
+    // ---------------- CREATE ----------------
+
+    public void insertBeginning(String nameIB, int powerIB, String villageIB){
+        NinjaNode newNinja = new NinjaNode(nameIB, powerIB, villageIB);
         newNinja.next = head;
         head = newNinja;
         size++;
     }
-    public  void insertEnd(String nameIE, int powerIE, String villageIE){
+
+    public void insertEnd(String nameIE, int powerIE, String villageIE){
         NinjaNode newNinja = new NinjaNode(nameIE, powerIE, villageIE);
         if (isempty()){
             head = newNinja;
@@ -52,34 +54,45 @@ public class ListaLigadaNinja {
         }
         size++;
     }
+
+    // ---------------- DELETE ----------------
+
     public void removeBeginning(){
         if (isempty()){
             System.out.println("vazio!");
+            return;
         }
         head = head.next;
         size--;
     }
-    public  void removeForName(String name){
+
+    public void removeForName(String name){
         if (isempty()){
             System.out.println("vazio");
+            return;
         }
+
         if (head.name.equals(name)){
             removeBeginning();
+            return;
         }
+
         NinjaNode actual = head;
         while (actual.next != null && !actual.next.name.equals(name)){
             actual = actual.next;
         }
+
         if (actual.next != null){
             actual.next = actual.next.next;
             size--;
             System.out.println(name + " removido da missão");
-
-        }
-        else {
+        } else {
             System.out.println("Não encontrado");
         }
     }
+
+    // ---------------- READ ----------------
+
     public NinjaNode searchForName(String name){
         NinjaNode actual = head;
         while (actual != null){
@@ -90,22 +103,50 @@ public class ListaLigadaNinja {
         }
         return null;
     }
+
     public void display(){
         if (isempty()){
             System.out.println("Não existe Ninja ainda!");
+            return;
         }
+
         System.out.println("----------EQUIPE----------");
-        NinjaNode actual  = head;
+        NinjaNode actual = head;
         int pos = 0;
 
         while (actual != null){
-            System.out.println( pos + 1 + ". " + actual.name + " PODER: " + actual.powerChakra
-                    + " Aldeia: "+ actual.village);
+            System.out.println(
+                    (pos + 1) + ". " + actual.name +
+                            " | PODER: " + actual.powerChakra +
+                            " | Aldeia: " + actual.village
+            );
             actual = actual.next;
             pos++;
         }
-        System.out.println("quantidade de ninjas:" + size);
+        System.out.println("Quantidade de ninjas: " + size);
     }
 
-}
+    // ---------------- UPDATE ----------------
 
+    public void updateForName(String name, String newName, int newPower, String newVillage){
+        if (isempty()){
+            System.out.println("Lista vazia!");
+            return;
+        }
+
+        NinjaNode actual = head;
+
+        while (actual != null){
+            if (actual.name.equals(name)){
+                actual.name = newName;
+                actual.powerChakra = newPower;
+                actual.village = newVillage;
+                System.out.println("Ninja atualizado com sucesso!");
+                return;
+            }
+            actual = actual.next;
+        }
+
+        System.out.println("Ninja não encontrado para atualização.");
+    }
+}
